5712bccb7b082de333700419a27968a22af0cc24
midCenturyDude7 <mgriffes@gmail.com>
2023-03-21 21:53:54 -0700

    Set up Navbar and Sidebar components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+import { useSelector } from "react-redux";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Dashboard from "./scenes/dashboard/index";
+import Layout from "./scenes/layout/index";
 
 function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = React.useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div>
-      <h1>MERN-React-Admin-Dashboard Application</h1>
+      <Router>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </Router>
     </div>
   );
 }
